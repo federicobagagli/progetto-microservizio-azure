68fa82c9a8b5d88d5bb161c647a24327625d37b3
federicobagagli <federik1982@gmail.com>
2025-04-23 12:52:02 +0200

    tentativo con cross origin verso il frontend su api all

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 // Permette l'accesso solo dal tuo dominio specifico
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")  // Sostituisci con l'URL del tuo front-end
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://proud-pebble-0d5b12f03.6.azurestaticapps.net"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);  // Se hai bisogno di inviare credenziali (cookies, sessioni, etc.)
